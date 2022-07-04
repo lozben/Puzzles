9f0a2cbce1209da99a135b909b481301e150d658
lozben <lozben88@gmail.com>
2022-07-04 09:57:59 +0200

    First gcd, then primes

--- Prime_operations.py
+++ Prime_operations.py
@@ -1,45 +1,30 @@
 import numpy as np
 from decimal import Decimal
+from math import gcd
+
+def count_primes(n):
+    p = 0
+
+    for i in range(2, int(np.sqrt(n)) + 1):
+        while n % i == 0 :
+            n = n / i 
+            p += 1
+    if n != 1:
+        p += 1
+    
+    return p
 
 def prime_operations(x, y):
-    out = {}
-    Pair = [x, y]
-    Pair.sort()
-    M = Pair[1]
-    m = Pair[0]
-    sqroot = int(Decimal(M).sqrt()) + 1
-    sieve = [True]*sqroot
-    keys = out.keys
-    for p in range(2, sqroot):
-        if (sieve[p]):
-            while M % p == 0 or m % p == 0:
-                if m == M:
-                    break
-                if M % p == 0:
-                    M = M / p
-                    if p not in keys():
-                        out[p] = 1
-                    else:
-                        out[p] += 1
-                elif m % p == 0:
-                    m = m / p
-                    if p not in keys():
-                        out[p] = -1
-                    else:
-                        out[p] -= 1
+    d = gcd(int(x), int(y))
+    x = x // d
+    y = y // d
 
-            for i in range(p, sqroot, p):
-                sieve[i] = False
-                
-    if m != 1 and M != m:
-        out[m] = 1
-    if M != 1 and M != m:
-        out[M] = 1
-    out_list = np.sum(list(map(abs, out.values())))
-    return out_list
+    return count_primes(x) + count_primes(y)
+
 	
 from datetime import datetime
 start_time = datetime.now()
 print(prime_operations(1e+12, 9.7836473e+11))
 end_time = datetime.now()
-print('Duration version Katya: {}'.format(end_time - start_time))
+print('Duration version 1: {}'.format(end_time - start_time))
+
